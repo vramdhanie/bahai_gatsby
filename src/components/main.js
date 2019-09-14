e97dcccda1902694046b5420bf1bd910789ef78d
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-09-13 20:32:32 -0400

    Add main component

---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components"
+
+const Main = ({ className, children }) => {
+  return <main className={className}>{children}</main>
+}
+
+export default styled(Main)`
+  margin: -60px 30px 0px;
+  background: #ffffff;
+  box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14),
+    0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
+  border-radius: 6px;
+  position: relative;
+  z-index: 1;
+  flex-grow: 2;
+`
