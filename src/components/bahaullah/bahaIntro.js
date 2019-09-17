07cadda4fb3ad612169424fd8b7b8b283b3bfb15
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-09-16 23:23:46 -0400

    Add more placeholder content for some pages

--- src/components/bahaullah/bahaIntro.js
+++ src/components/bahaullah/bahaIntro.js
@@ -5,7 +5,22 @@ const BahaIntro = ({ className }) => {
   return (
     <section className={className}>
       <Title title="Bahá’u’lláh" subtitle="" />
-      <p>The Promised One foretold by all the Divine Messengers</p>
+      <p>
+        The Bahá’í Faith began with the mission entrusted by God to two Divine
+        Messengers—the Báb and Bahá’u’lláh. Today, the distinctive unity of the
+        Faith They founded stems from explicit instructions given by Bahá’u’lláh
+        that have assured the continuity of guidance following His passing. This
+        line of succession, referred to as the Covenant, went from Bahá’u’lláh
+        to His Son ‘Abdu’l-Bahá, and then from ‘Abdu’l-Bahá to His grandson,
+        Shoghi Effendi, and the Universal House of Justice, ordained by
+        Bahá’u’lláh. A Bahá’í accepts the divine authority of the Báb and
+        Bahá’u’lláh and of these appointed successors.
+      </p>
+      <div class="follow_link">
+        <a href="https://www.bahai.org/bahaullah/" class="btn-primary">
+          Read more about the life of Bahá’u’lláh.
+        </a>
+      </div>
       <blockquote>
         Bahá’u’lláh has drawn the circle of unity, He has made a design for the
         uniting of all the peoples, and for the gathering of them all under the
@@ -25,4 +40,8 @@ export default styled(BahaIntro)`
   width: 80%;
   max-width: 800px;
   margin: 0 auto;
+
+  .follow_link {
+    text-align: center;
+  }
 `
--- src/components/bicentenary/bicentenaryIntro.js
+++ src/components/bicentenary/bicentenaryIntro.js
@@ -1,12 +1,49 @@
 import React from "react"
 import styled from "styled-components"
 import Title from "../title"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const getImage = graphql`
+  query image {
+    fluid: file(relativePath: { eq: "bicentenary_02.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const BicentenaryIntro = ({ className }) => {
+  const {
+    fluid: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(getImage)
   return (
     <section className={className}>
       <Title title="Bicentenary of" subtitle="the birth of the Báb" />
-      <p>Celebrating the 200th anniversary of The birth of The Báb.</p>
+      <div className="image">
+        <Img fluid={fluid} alt="Bicentenary Logo" />
+      </div>
+      <p>
+        On 29 AND 30 October 2019, across Trinidad and Tobago and in countless
+        settings and places throughout the world, people will celebrate the
+        200th Anniversary of the Birth of The Báb, whose revitalizing message
+        prepared the way for the coming of Bahá’u’lláh, the founder of the
+        Bahá’í Faith.
+      </p>
+      <p>
+        Stay tuned to this page for information about the celebrations happening
+        in your community.
+      </p>
+      <div class="follow_link">
+        <a href="https://bicentenary.bahai.org/the-bab/" class="btn-primary">
+          See how the World Celebrates
+        </a>
+      </div>
       <blockquote>
         Verily, we have believed in Thee and in Thy signs ere the dawn of Thy
         Manifestation, and in Thee are we all well assured. Verily, we have
@@ -31,4 +68,19 @@ export default styled(BicentenaryIntro)`
   width: 80%;
   max-width: 800px;
   margin: 0 auto;
+  .image {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  @media screen and (min-width: 480px) {
+    .image {
+      width: 60%;
+      max-width: 600px;
+    }
+  }
+
+  .follow_link {
+    text-align: center;
+  }
 `
