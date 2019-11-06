5b417681e0efe5bc03cb26667dbbd01b7b6f96db
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-11-06 12:09:03 -0400

    Create generic page section components

---
+++ src/components/whiteSection.js
@@ -0,0 +1,44 @@
+import React from "react"
+import styled from "styled-components"
+import Title from "./title"
+
+const WhiteSection = ({ className, title, subtitle, children }) => {
+  return (
+    <section className={className}>
+      <Title title={title} subtitle={subtitle} />
+      <div className="feature-center">{children}</div>
+    </section>
+  )
+}
+
+export default styled(WhiteSection)`
+  font-weight: 300;
+  padding: 4rem 0;
+
+  .feature-center {
+    width: 80%;
+    margin: 0 auto;
+    max-width: 800px;
+  }
+
+  border-bottom: solid 2px rgba(219, 132, 10, 0.2);
+  background: var(--mainWhite);
+
+  @media screen and (min-width: 768px) {
+    .feature-center {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-column-gap: 3rem;
+      grid-row-gap: 1rem;
+      align-items: center;
+      margin-top: 3rem;
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    .feature-center {
+      width: 90vw;
+      max-width: 1170px;
+    }
+  }
+`
