7f8fcef20e2289cf4bb6ae641289e89ba0adc9fd
Vincent Ramdhnaie <vramdhanie@gmail.com>
2021-08-11 11:06:42 -0400

    Create empty history page

---
+++ src/pages/history.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const History = () => {
+    return (
+        <Layout>
+            <SEO title="The History of the Baha'i Faith in Trinidad and Tobago"/>
+
+        </Layout>
+    )
+}
+
+export default styled(History)``
